90b159dd05159c0ea352734bcfc6fa113a61664b
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-02-10 16:20:34 +0700

    Belajar Dasar Input dan Penjumlahan C#

---
+++ first-try/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SMKCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama;
+            int umur;
+            Console.Write("Masukkan Nama : ");
+            nama = Console.ReadLine();
+            Console.Write("Masukkan Umur : ");
+            umur = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nama Kamu : " + nama + ", Umur Kamu : " + umur);
+        }
+    }
+}
---
+++ penjumlahan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SMKCoding
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int a = 10, b = 2;
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
+// JAWABAN SMKCODING TUGAS PENJUMLAHAN
+
+// class studio 
+// {
+//     public static int run(int a, int b) 
+//     { 
+//         return a + b; 
+//     } 
+// }
